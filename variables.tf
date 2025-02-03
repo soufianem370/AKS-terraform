6fcbdf2cddf0d37d7915c11b311db3d6ffc44a6a
soufiane makhloufi <45273948+soufianem370@users.noreply.github.com>
2025-02-02 23:26:37 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+# variables.tf
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+}
